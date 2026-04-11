d8825b774cfa61e6fab02d9feafaad327725a794
PL-FE <41770773+PL-FE@users.noreply.github.com>
2026-04-11 05:37:01 +0000

    Deploying to gh-pages from @ PL-FE/DesignKitWeb@415256f259e760ca170163d24d7e76dfa34dec53 🚀

---
+++ assets/video-CKV35gno.js
@@ -0,0 +1,1 @@
+import{a2 as p,D as e}from"./index-DAW_uh82.js";const d=p("video",()=>{const r=e("mp4"),o=e("medium"),s=e("gif"),t=e(15);return{preferredFormat:r,preferredCompressLevel:o,preferredGifFormat:s,preferredFps:t}},{persist:!0});export{d as u};
